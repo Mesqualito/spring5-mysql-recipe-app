e5ea8e092f5bf8255244bdc1e497b770cba6d190
Mesqualito <jochen@gebsattel.rocks>
2020-09-03 15:59:32 +0200

    analog zu 'https://www.udemy.com/course/spring-framework-5-beginner-to-guru/learn/lecture/7704854' - Lektion 18/307

---
+++ src/main/scripts/configure-mysql.sql
@@ -0,0 +1,33 @@
+## Use to run mysql db docker image, optional if you're not using a local mysqldb
+# only dev: docker run --name mysqldb -p 3306:3306 --cap-add SYS_NICE -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
+# prod min.: docker run --name mysqldb -p 3306:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql
+
+# connect to mysql and run as root user
+#Create Databases
+CREATE DATABASE sfg_dev;
+CREATE DATABASE sfg_prod;
+
+#Create database service accounts
+CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'password';
+CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'password';
+
+#Database grants
+# DML ("schema privileges") only, no DDL-capabilities ("Administrative roles") for the users
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
